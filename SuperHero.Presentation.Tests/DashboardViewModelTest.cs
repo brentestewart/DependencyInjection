d7472be8a5363e9300f43932fd4ce1e75453da92
Brent Stewart <brent@alienarc.com>
2017-08-01 21:58:36 -0500

    Added Test Project

---
+++ SuperHero.Presentation.Tests/DashboardViewModelTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SuperHero.Common;
+
+namespace SuperHero.Presentation.Tests
+{
+    [TestClass]
+    public class DashboardViewModelTest
+    {
+        public Mock<IHeroRepository> HeroMock { get; set; }
+        public IHeroRepository HeroRepository { get; set; }
+        [TestInitialize]
+        public void Initialize()
+        {
+            HeroMock = new Mock<IHeroRepository>();
+            HeroMock.Setup(r => r.GetAllHeroes()).Returns(new List<IHero>());
+            HeroRepository = HeroMock.Object;
+
+        }
+
+        [TestMethod]
+        public void Constructor_ShouldCallRepositoryGetAllHeroesMethod()
+        {
+            var viewModel = new DashboardViewModel(HeroRepository);
+            HeroMock.Verify(r => r.GetAllHeroes(), Times.Once);
+        }
+    }
+}
---
+++ SuperHero.Presentation.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SuperHero.Presentation.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SuperHero.Presentation.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("085b0027-93e4-4854-8991-03735c8709e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
