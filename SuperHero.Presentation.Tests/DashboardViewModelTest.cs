b7ff32aa9154b5d03002f3b9ab14331bb0dc717a
Brent Stewart <brent@alienarc.com>
2017-08-01 22:13:33 -0500

    Test Cleanup

--- SuperHero.Presentation.Tests/DashboardViewModelTest.cs
+++ SuperHero.Presentation.Tests/DashboardViewModelTest.cs
@@ -9,27 +9,15 @@ namespace SuperHero.Presentation.Tests
     [TestClass]
     public class DashboardViewModelTest
     {
-        public Mock<IHeroRepository> HeroMock { get; set; }
-        public IHeroRepository HeroRepository { get; set; }
         [TestInitialize]
         public void Initialize()
         {
-            HeroMock = new Mock<IHeroRepository>();
-            HeroRepository = HeroMock.Object;
-
         }
 
         [TestMethod]
-        public void Constructor_ShouldCallRepositoryGetAllHeroesMethod_Always()
+        public void Constructor_ShouldCallRepositoryGetAllHeroesMethodOnce_Always()
         {
-            //Arange
-            HeroMock.Setup(r => r.GetAllHeroes()).Returns(new List<IHero>());
-            
-            //Act
-            var viewModel = new DashboardViewModel();
-
-            //Assert
-            HeroMock.Verify(r => r.GetAllHeroes(), Times.Once);
+            // How can we test this?
         }
     }
 }
