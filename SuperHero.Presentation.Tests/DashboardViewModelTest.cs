454288bd6d671335812a533da1e6a4ea6f12db8d
Brent Stewart <brent@alienarc.com>
2017-08-04 01:23:38 -0500

    More test updates

--- SuperHero.Presentation.Tests/DashboardViewModelTest.cs
+++ SuperHero.Presentation.Tests/DashboardViewModelTest.cs
@@ -10,27 +10,46 @@ namespace SuperHero.Presentation.Tests
     [TestClass]
     public class DashboardViewModelTest
     {
-        public IHeroRepository HeroRepository { get; set; }
-        public IHero Hero { get; set; }
-
+ 
         [TestInitialize]
         public void Initialize()
         {
-            var mockRepo = new Mock<IHeroRepository>();
-            HeroRepository = mockRepo.Object;
-            var heroMock = new Mock<IHero>();
-            heroMock.Setup(h => h.HeroName).Returns("Mock Hero");
-            Hero = heroMock.Object;
-            mockRepo.Setup(r => r.GetAllHeroes()).Returns(new List<IHero> {Hero});
         }
 
         [TestMethod]
         public void Constructor_ShouldCallRepositoryGetAllHeroesMethodOnce_Always()
         {
             // How can we test this?          
-            var viewModel = new DashboardViewModel(HeroRepository);
-            Assert.AreEqual(1, viewModel.AvailableHeroes.Count);
-            Assert.AreEqual("Mock Hero", viewModel.AvailableHeroes.First().HeroName);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public IHeroRepository HeroRepository { get; set; }
+//public IHero Hero { get; set; }
+
+
+
+//var mockRepo = new Mock<IHeroRepository>();
+//HeroRepository = mockRepo.Object;
+//var heroMock = new Mock<IHero>();
+//heroMock.Setup(h => h.HeroName).Returns("Mock Hero");
+//Hero = heroMock.Object;
+//mockRepo.Setup(r => r.GetAllHeroes()).Returns(new List<IHero> {Hero});
+
+
+
+//var viewModel = new DashboardViewModel(HeroRepository);
+//Assert.AreEqual(1, viewModel.AvailableHeroes.Count);
+//Assert.AreEqual("Mock Hero", viewModel.AvailableHeroes.First().HeroName);
